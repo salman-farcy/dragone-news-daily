3e9f60f6320442942713adb309d1df189b8ec6c3
salman-farcy <salmanfarcy253@gmail.com>
2024-02-26 22:42:12 +0600

    The data has been added by mapping

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -1,4 +1,6 @@
 import { getCategoryNews } from "@/utils/getAllCategoriesNews";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import Image from "next/image";
 
 
 const DynamicNewsPage = async ({ params, searchParams }) => {
@@ -11,6 +13,36 @@ const DynamicNewsPage = async ({ params, searchParams }) => {
      return (
           <div className="my-3">
                <h1>Total <b className="uppercase">{searchParams.category}</b> News : <span className="font-bold">{data.length}</span> </h1>
+
+               <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    {
+                         data.map(news => (
+                              <Grid key={news.id} item xs={6}>
+                                   <Card className="my-5">
+                                        <CardMedia>
+                                             <Image className="w-full" src={news.thumbnail_url} width={800} height={500} alt="top news" />
+                                        </CardMedia>
+                                        <CardContent>
+                                             <p className="p-1 inline-block rounded-sm bg-red-500 text-white">{news?.category}</p>
+                                             <Typography gutterBottom>
+                                                  {news.title}
+                                             </Typography>
+                                             <Typography gutterBottom variant="h5" component="div">
+                                                  By {news.author.name} {news.author.published_date}
+                                             </Typography>
+                                             <Typography variant="body2" color="text.secondary">
+                                                  {news.details}
+                                             </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                             <Button size="small">Share</Button>
+                                             <Button size="small">Learn More</Button>
+                                        </CardActions>
+                                   </Card>
+                              </Grid>
+                         ))
+                    }
+               </Grid>
           </div>
      );
 };
