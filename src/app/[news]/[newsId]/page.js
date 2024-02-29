38680527402dc17ae75ec1dfc11518457f4d4bf7
salman-farcy <salmanfarcy253@gmail.com>
2024-02-29 21:43:40 +0600

    not-found/loading/404 page create

--- src/app/[news]/[newsId]/page.js
+++ src/app/[news]/[newsId]/page.js
@@ -1,14 +1,14 @@
 import { getSingleNews } from "@/utils/getSingleNews";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 
 
 const NewsDetailPage = async ({ params }) => {
-     const { data:news } = await getSingleNews(params.newsId);
+     const { data: news } = await getSingleNews(params.newsId);
      console.log(news);
 
      return (
-          <Box>
+          <Box className="my-5">
                <Container>
                     <Grid container spacing={2}>
                          <Grid item lg={6}>
@@ -22,8 +22,34 @@ const NewsDetailPage = async ({ params }) => {
                                    </Grid>
                               </Grid>
                          </Grid>
-                         <Grid item lg={6}>
-                              <Typography variant="h5">{news.title}</Typography>
+                         <Grid item lg={6} className="space-y-3">
+                              <Typography variant="h5" component={"h2"}>{news.title}</Typography>
+                              <Box sx={{
+                                   display: 'flex',
+                                   gap: 1,
+                                   alignItems: 'center'
+                              }}>
+                                   <Avatar alt="author" src={news.author.img} />
+                                   <Typography>
+                                        By {news.author.name}
+                                   </Typography>
+                                   <Typography>
+                                        - {news.author.published_date}
+                                   </Typography>
+                              </Box>
+                              <Typography style={{
+                                   textAlign: "justify",
+                                   whiteSpace: "pre-line",
+                                   color: "gray"
+                              }}>{news.details}</Typography>
+
+                              <Typography variant="h6">
+                                   ``Many desktop publishing packages and web page editors now use as their default model test!
+                              </Typography>
+
+                              <Typography variant="h6">
+                                  -Salman farcy
+                              </Typography>
                          </Grid>
                     </Grid>
                </Container>
